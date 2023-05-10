4f72ffc396bc9795029010f8778e0a766d26843e
Murken <muarvievvlad.2002@gmail.com>
2023-05-10 03:45:26 +0300

    mapping

--- Users.WebApi/Controllers/UserController.cs
+++ Users.WebApi/Controllers/UserController.cs
@@ -1,9 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Users.Domain;
+using Users.Application.Users.Queries.GetUserList;
+using Users.Application.Users.Queries.GetOneUser;
 
 namespace Users.WebApi.Controllers
 {
 	public class UserController : BaseController
 	{
-		
+		[HttpGet]
+		public async Task<ActionResult<IList<User>>> GetAll()
+		{
+			var query = new GetUserListQuery();
+			var vm = await Mediator.Send(query);
+			return Ok(vm);
+		}
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<User>> Get(Guid id)
+		{
+			var query = new GetOneUserQuery()
+			{
+				Id = id
+			};
+			var vm = await Mediator.Send(query);
+			return Ok(vm);
+		}
+
+		[HttpPost]
+		public async Task<ActionResult<Guid>> Create([FromBody])
 	}
 }
